c405e5ecb844e997b997e303a16d4d46dfca50d1
alaqsaka <aqsaabidin15@gmail.com>
2021-04-04 17:44:07 +0700

    commit 2

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -24,8 +24,30 @@ const Title = styled.p`
   color: #04050a;
   font-weight: 400;
 `;
-const Desc = styled.p``;
-const Button = styled.a``;
+const Desc = styled.p`
+  width: 472px;
+  font-size: 20px;
+  color: #9ea0ac;
+  line-height: 30px;
+  margin-top: 58px;
+`;
+const Button = styled.a`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 18px;
+  margin-top: 58px;
+  width: 371px;
+  height: 71px;
+  line-height: 71px;
+  font-size: 22px;
+  text-align: center;
+  color: #fff;
+  cursor: pointer;
+  background: linear-gradient(90deg, #0546d6, #3f89fc);
+  text-decoration: none;
+  box-shadow: 0 15px 14px rgb(0 42 177 / 12%);
+`;
 
 const Hero = () => {
   return (
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,30 @@
 import React from "react";
+import styled from "styled-components";
+import Icon from "../assets/logo.svg";
 
 const Navbar = () => {
   return (
-    <div>
-      <h1>Navvv</h1>
-    </div>
+    <NavbarConainter>
+      <NavbarWrap>
+        <Nav>
+          <NavLink to="/">HOME</NavLink>
+          <NavLink to="/">MARKET</NavLink>
+          <NavLink to="/">TRADE</NavLink>
+          <NavLink to="/">PRICING</NavLink>
+          <NavLink to="/">DOWNLOAD</NavLink>
+          <NavLink to="/">HELP</NavLink>
+        </Nav>
+        <Search>
+          <SearchWrap>
+            <Input type="text" placeholder="Symbol/Name" />
+          </SearchWrap>
+        </Search>
+        <ButtonContainer>
+          <Button>SIGN UP</Button>
+          <Button>LOG IN</Button>
+        </ButtonContainer>
+      </NavbarWrap>
+    </NavbarConainter>
   );
 };
 
